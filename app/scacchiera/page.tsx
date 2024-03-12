1f43d47db5753597d85230e4b70bae7ee32cf40d
LucaForamacchi <foramacchicico02@gmail.com>
2024-03-12 11:26:46 +0100

    Update page.tsx

--- app/scacchiera/page.tsx
+++ app/scacchiera/page.tsx
@@ -1,32 +1,44 @@
 // To inform next js, this is a client component 
 "use client"; 
 import React, { useState } from 'react';
-// Definizione del componente Home
+
 export default function Home() {
   const [cells, setCells] = useState<number[][]>([
-  [5, 3, 3, 9, 10, 3, 3, 5],
-  [1, 1, 1, 1, 1, 1, 1, 1],
-  [0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0],
-  [1, 1, 1, 1, 1, 1, 1, 1],
-  [5, 3, 3, 9, 10, 3, 3, 5]
-]);
-  
+    [5, 3, 3, 9, 10, 3, 3, 5],
+    [1, 1, 1, 1, 1, 1, 1, 1],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [1, 1, 1, 1, 1, 1, 1, 1],
+    [5, 3, 3, 9, 10, 3, 3, 5]
+  ]);
   const [selectedCell, setSelectedCell] = useState<{ rowIndex: number, colIndex: number } | null>(null);
-  const [previousCell, setPreviousCell] = useState<{ rowIndex: number, colIndex: number } | null>(null);
+  const [moves, setMoves] = useState<string[]>([]); 
 
   const handleCellClick = (rowIndex: number, colIndex: number) => {
     if (selectedCell) {
-      // Scambio dei numeri tra le due caselle
-      const temp = cells[selectedCell.rowIndex][selectedCell.colIndex];
       const newCells = [...cells];
-      newCells[selectedCell.rowIndex][selectedCell.colIndex] = cells[rowIndex][colIndex];
+      const temp = newCells[selectedCell.rowIndex][selectedCell.colIndex];
+
+      newCells[selectedCell.rowIndex][selectedCell.colIndex] = newCells[rowIndex][colIndex];
       newCells[rowIndex][colIndex] = temp;
+
+      if (newCells[rowIndex][colIndex] !== 0) {
+        newCells[selectedCell.rowIndex][selectedCell.colIndex] = 0;
+        
+        const move = `${selectedCell.rowIndex + 1}${selectedCell.colIndex + 1} => ${rowIndex + 1}${colIndex + 1}`;
+        
+        // Se la mossa implica una mangiata, aggiungi il "+" alla fine
+        if ((Math.abs(selectedCell.rowIndex - rowIndex) > 1 || Math.abs(selectedCell.colIndex - colIndex) > 1)&&temp!=0) {
+          setMoves(prevMoves => [...prevMoves, move + "+"]);
+        } else {
+          setMoves(prevMoves => [...prevMoves, move]);
+        }
+      }
+      
       setCells(newCells);
-      setPreviousCell(selectedCell);
-      setSelectedCell({ rowIndex, colIndex });
+      setSelectedCell(null);
     } else {
       setSelectedCell({ rowIndex, colIndex });
     }
@@ -34,35 +46,22 @@ export default function Home() {
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      {/* Griglia di 8x8 celle */}
       <div className="grid grid-cols-8">
-        {/* Generazione dinamica delle celle della griglia */}
         {cells.map((row, rowIndex) => (
-          // Generazione dinamica delle colonne
           row.map((cell, colIndex) => (
             <div
-              key={`${rowIndex}-${colIndex}`} // Chiave unica per ogni cella
-              className={`p-4 ${rowIndex % 2 === colIndex % 2 ? 'bg-gray-200' : 'bg-gray-400'} w-12 h-12`} // Applicazione di classi CSS in base alla posizione della cella
-              onClick={() => handleCellClick(rowIndex, colIndex)} // Gestore del clic sulla cella
+              key={`${rowIndex}-${colIndex}`}
+              className={`p-4 ${rowIndex % 2 === colIndex % 2 ? 'bg-gray-200' : 'bg-gray-400'} w-12 h-12`}
+              onClick={() => handleCellClick(rowIndex, colIndex)}
             >
-              {/* Testo all'interno della cella, calcolato in base alla posizione */}
               {cell !== 0 ? cell : ""}
             </div>
           ))
         ))}
       </div>
-      {/* Visualizzazione del numero della cella cliccata */}
-      {selectedCell && (
-        <div className="mt-4">
-          Cell clicked: {cells[selectedCell.rowIndex][selectedCell.colIndex]}
-        </div>
-      )}
-      {/* Visualizzazione della cella precedente */}
-      {previousCell && (
-        <div className="mt-4">
-          Previous cell: {cells[previousCell.rowIndex][previousCell.colIndex]}
-        </div>
-      )}
+      <div className="mt-4">
+        Mosse effettuate: {moves.join(' , ')}
+      </div>
     </main>
   );
 }
